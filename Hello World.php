239339ba85b5e2e3579aa1c56f853f620107d11c
elliot <elliot.a@gmail.com>
2009-07-11 00:10:58 -0700

    Fixed indentation issue

--- Hello World.php
+++ Hello World.php
@@ -4,11 +4,11 @@ array_map ( function ( $seed, $fill )
             {
                 mt_srand ( $seed );
                 $word = vsprintf ( str_repeat ( '%c', $fill ) . ' ',
-		                          array_map ( function ( $void ) { return mt_rand ( 1, 26) + 96; },
-		                                      range ( 1, $fill )
-		                          )
-		                );
-				echo ucfirst ( $word );
+                                    array_map ( function ( $void ) { return mt_rand ( 1, 26) + 96; },
+                                                range ( 1, $fill )
+                                    )
+                        );
+                echo ucfirst ( $word );
             },
             array ( -2129053370, -2142903234 ), 
             array (  5, 5 )
